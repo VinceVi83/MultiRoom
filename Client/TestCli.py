29df6712635d81d4cbed35a8a5c77688942c5ba1
VinceVi83 <vincent@nguyen.lt>
2017-06-28 01:52:26 +0200

    Commit progress of the project. Music.py finish for this phase. Service finish for this phase. Maybe I will add about manage playlist but it's a bonus for this phase. InterfaceSerCli need some modification to don't use username as head of message.. I have an idea link ip with username. After it will be finish this phase.
    commit TestCli with a test for check if test of Music.py is OK/KO (in comment). so, it's still usable for manual command of VLC.

--- Client/TestCli.py
+++ Client/TestCli.py
@@ -2,7 +2,7 @@
 # coding: utf-8
 
 import socket
-
+import time
 hote = "127.0.0.1"
 port = 8888
 
@@ -10,11 +10,30 @@ socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 socket.connect((hote, port))
 print("Connection on {}".format(8888))
 keep = True
+# Authentification
 socket.send("toto.toto".encode())
+time.sleep(1)
+
+
+# Manual use
 while(keep):
     msg = "toto." + input()
     socket.send(msg.encode())
     if msg == "Fin":
         keep = False
+
+""" Test Purpose
+socket.send("toto.VLC.start./run/media/vinsento/455B532D7757A5FD/Project/Test/".encode())
+time.sleep(1)
+socket.send("toto.VLC.random".encode())
+time.sleep(1)
+socket.send("toto.VLC.next".encode())
+time.sleep(1)
+while(keep):
+    socket.send("toto.Music.info".encode())
+    input()
+    socket.send("toto.VLC.next".encode())
+    time.sleep(8)
+"""
 
 socket.close()
